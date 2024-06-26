8856b4e64fceebd0e88d72eb66db77e9360ac878
AnastasiyaS92 <anastasiya_s@mail.ru>
2024-06-26 15:34:53 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = "Курс: Python,"
+first_name = "Всего задач:"
+first_value = 12
+second_name = "затрачено часов:"
+second_value = 1.5
+third_name = "среднее время выполнения"
+third_value = second_value/first_value
+print(name, first_name, first_value, ',', second_name, second_value, ',', third_name, third_value, 'часа')
